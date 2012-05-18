0aed3e19230814dbb184c1ce163ad2f6b80b42b7
unknown <mikebruzina@.carfax.cfx>
2012-05-18 15:55:01 -0500

    more stuff

--- messingWithGroovy/src/com/examples/groovy/GroovyScratch.groovy
+++ messingWithGroovy/src/com/examples/groovy/GroovyScratch.groovy
@@ -1,8 +1,18 @@
 package com.examples.groovy
-
+@Grab(group='org.ccil.cowan.tagsoup', module='tagsoup', version='1.2' )
 class GroovyScratch {
 
 	def doSomething(){
 		"Here is something"
+	}
+
+	def fetchSomeHtml(){
+		def tagsoupParser = new org.ccil.cowan.tagsoup.Parser()
+		def slurper = new XmlSlurper(tagsoupParser)
+
+		slurper.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false)
+		def htmlParser = slurper.parse("http://www.usacycling.org/results/?compid=282794")
+
+		htmlParser.'**'.findAll{ it.@class == 'homearticleheader'}.each{ println it }
 	}
 }
--- messingWithGroovy/test/com/examples/groovy/GroovyScratchTest.groovy
+++ messingWithGroovy/test/com/examples/groovy/GroovyScratchTest.groovy
@@ -8,10 +8,11 @@ class GroovyScratchTest {
 
 	def groovyScratch = new GroovyScratch()
 	
-	@Test
-	public void doSomethingWorks(){
+	@Test public void doSomethingWorks(){
 		assert "Here is something" == groovyScratch.doSomething()
 	}
 	
-	
+	@Test public void tryGettingSomeHtml(){
+		groovyScratch.fetchSomeHtml()
+	}
 }
